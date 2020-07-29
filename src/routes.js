8d65e25296f78302aa778ce4d9e3ddabac7f9ad4
Rodrigo Fernandes <digonalha@gmail.com>
2020-07-29 17:03:20 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,6 @@ import DeliveryController from './app/controllers/DeliveryController';
 
 const routes = new Router();
 
-routes.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Origin',
-    'https://entregas-capivara-frontend.herokuapp.com/'
-  );
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
 routes.post('/delivery', DeliveryController.create);
 routes.get('/delivery', DeliveryController.list);
 routes.get('/delivery/:id', DeliveryController.get);
